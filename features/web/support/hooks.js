e6385c2bfe86ed8bcd7b439dc1ac87ed00b2a70d
edgariel <edgariel2004@gmail.com>
2022-11-20 00:43:56 -0500

    adiciona logica para imprimir con buen formato los pasos de cada escenario

--- features/web/support/hooks.js
+++ features/web/support/hooks.js
@@ -1,6 +1,8 @@
-const { After, Before } = require('@cucumber/cucumber');
+const { After, Before, AfterStep,BeforeStep,BeforeAll } = require('@cucumber/cucumber');
 const { WebClient } = require('kraken-node');
+const {existsSync, mkdirSync} = require("fs");
 
+var counter=0;
 Before(async function() {
   this.deviceClient = new WebClient('chrome', {}, this.userId);
   this.driver = await this.deviceClient.startKrakenForUserId(this.userId);
@@ -8,4 +10,22 @@ Before(async function() {
 
 After(async function() {
   await this.deviceClient.stopKrakenForUserId(this.userId);
+});
+
+AfterStep(async function(scenario) {
+  console.log("##############"+counter);
+  let escenarioPath = "./reports/screenshots/"+scenario.pickle.name.replace(/\s/g, '');
+  if (!existsSync(escenarioPath)) {
+      mkdirSync(escenarioPath, { recursive: true });
+  }
+  await this.driver.saveScreenshot("./reports/screenshots/" + 
+      scenario.pickle.name.replace(/\s/g, '') + "/Paso_" + counter + ".png")
+});
+
+BeforeStep(async function(scenario) {
+  counter = counter + 1;
+});
+
+BeforeAll(async function() {
+  counter = 0;
 });
